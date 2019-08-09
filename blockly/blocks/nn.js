7f358c168d6fe67dc167f7afa419e88f109e32ba
Boyuan Chen <boyuanchen@berkeley.edu>
2019-08-09 13:10:43 -0700

    add blockly

---
+++ blockly/blocks/nn.js
@@ -0,0 +1,127 @@
+Blockly.Blocks['nn_dense'] = {
+  init: function() {
+    this.appendDummyInput()
+        .appendField("FullyConnect ")
+        .appendField("In")
+        .appendField(new Blockly.FieldNumber(0, 0, 1024, 1), "in")
+        .appendField("Out")
+        .appendField(new Blockly.FieldNumber(0, 0, 1024, 1), "out")
+        .appendField("Activation")
+        .appendField(new Blockly.FieldDropdown([[{"src":"https://i.ibb.co/NmgSYPW/relu-1.png","width":20,"height":20,"alt":"*"},"relu"], [{"src":"https://i.ibb.co/1dVp7VR/tanh.png","width":20,"height":20,"alt":"*"},"tanh"], [{"src":"https://i.ibb.co/zm27KLn/sigmoid-1.png","width":20,"height":20,"alt":"*"},"sigmoid"], ["none","None"]]), "activation");
+    this.setInputsInline(true);
+    this.setOutput(true, null);
+    this.setColour(15);
+ this.setTooltip("");
+ this.setHelpUrl("");
+  }
+};
+
+Blockly.Blocks['nn_conv2d'] = {
+  init: function() {
+    this.appendDummyInput()
+        .appendField("Convolution")
+        .appendField("In")
+        .appendField(new Blockly.FieldNumber(0, 0, 1024, 1), "in")
+        .appendField("Out")
+        .appendField(new Blockly.FieldNumber(0, 0, 1024, 1), "out")
+        .appendField("kernel size")
+        .appendField(new Blockly.FieldNumber(3, 1, 1024, 1), "k")
+        .appendField("stride")
+        .appendField(new Blockly.FieldNumber(2, 1, 256, 1), "s")
+        .appendField("activation")
+        .appendField(new Blockly.FieldDropdown([[{"src":"https://i.ibb.co/NmgSYPW/relu-1.png","width":20,"height":20,"alt":"*"},"relu"], [{"src":"https://i.ibb.co/1dVp7VR/tanh.png","width":20,"height":20,"alt":"*"},"tanh"], [{"src":"https://i.ibb.co/zm27KLn/sigmoid-1.png","width":20,"height":20,"alt":"*"},"sigmoid"], ["none","None"]]), "activation");
+    this.setOutput(true, null);
+    this.setColour(15);
+ this.setTooltip("");
+ this.setHelpUrl("");
+  }
+};
+
+Blockly.Blocks['nn_guesscategory'] = {
+  init: function() {
+    this.appendDummyInput()
+        .appendField("GuessCategory");
+    this.setOutput(true, null);
+    this.setColour(15);
+ this.setTooltip("");
+ this.setHelpUrl("");
+  }
+};
+
+Blockly.Blocks['nn_sequential'] = {
+  init: function() {
+    this.appendValueInput("NAME")
+        .setCheck(null)
+        .appendField("build model with list");
+    this.setInputsInline(false);
+    this.setOutput(true, null);
+    this.setColour(90);
+ this.setTooltip("");
+ this.setHelpUrl("");
+  }
+};
+
+Blockly.Blocks['nn_forward'] = {
+  init: function() {
+    this.appendDummyInput()
+        .appendField("Predict with")
+        .appendField(new Blockly.FieldVariable("model"), "model")
+        .appendField(new Blockly.FieldVariable("data"), "data");
+    this.setInputsInline(false);
+    this.setOutput(true, null);
+    this.setColour(230);
+ this.setTooltip("");
+ this.setHelpUrl("");
+  }
+};
+
+Blockly.Blocks['nn_l1loss'] = {
+  init: function() {
+    this.appendDummyInput()
+        .appendField("Difference");
+    this.setOutput(true, null);
+    this.setColour(60);
+ this.setTooltip("");
+ this.setHelpUrl("");
+  }
+};
+
+Blockly.Blocks['nn_dataset'] = {
+  init: function() {
+    this.appendDummyInput()
+        .appendField("dataset")
+        .appendField(new Blockly.FieldDropdown([["object","cifar10"], ["number","mnist"], ["photo","imagenet"]]), "NAME");
+    this.setOutput(true, null);
+    this.setColour(230);
+ this.setTooltip("");
+ this.setHelpUrl("");
+  }
+};
+
+Blockly.Blocks['nn_backward'] = {
+  init: function() {
+    this.appendDummyInput()
+        .appendField("improve")
+        .appendField(new Blockly.FieldVariable("model"), "model")
+        .appendField("with")
+        .appendField(new Blockly.FieldVariable("data"), "data");
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
+    this.setColour(230);
+ this.setTooltip("");
+ this.setHelpUrl("");
+  }
+};
+
+
+Blockly.Blocks['next'] = {
+  init: function() {
+    this.appendDummyInput()
+        .appendField("next data")
+        .appendField(new Blockly.FieldVariable("dataset"), "dataset");
+    this.setOutput(true, null);
+    this.setColour(120);
+ this.setTooltip("");
+ this.setHelpUrl("");
+  }
+};
---
+++ blockly/generators/python/nn.js
@@ -0,0 +1,92 @@
+'use strict';
+
+goog.provide('Blockly.Python.nn');
+goog.require('Blockly.Python');
+
+Blockly.Python['nn_dense'] = function(block) {
+  var number_in = block.getFieldValue('in');
+  var number_out = block.getFieldValue('out');
+  var dropdown_activation = block.getFieldValue('activation');
+  // TODO: Assemble Python into code variable.
+  var code = 'nn.Dense(' + number_in + ', '+ number_out + ')';
+  // TODO: Change ORDER_NONE to the correct strength.
+  return [code, Blockly.Python.ORDER_NONE];
+};
+
+Blockly.Python['nn_conv2d'] = function(block) {
+  var number_in = block.getFieldValue('in');
+  var number_out = block.getFieldValue('out');
+  var number_k = block.getFieldValue('k');
+  var number_s = block.getFieldValue('s');
+  var dropdown_activation = block.getFieldValue('activation');
+  // TODO: Assemble Python into code variable.
+  var activation = '';
+  if (dropdown_activation == 'sigmoid') {
+    activation = ', nn.Sigmoid()';
+  }
+  if (dropdown_activation == 'tanh') {
+    activation = ', nn.Tanh()';
+  }
+  if (dropdown_activation == 'relu') {
+    activation = ', nn.ReLU()';
+  }
+  var code = 'nn.Conv2d(' + number_in + ', '+ number_out + ', ' + number_k + ', ' + number_s + ', ' + ~~(number_k / 2) +  ')' + activation;
+  // TODO: Change ORDER_NONE to the correct strength.
+  return [code, Blockly.Python.ORDER_NONE];
+};
+
+Blockly.Python['nn_guesscategory'] = function(block) {
+  // TODO: Assemble Python into code variable.
+  var code = 'nn.Softmax()';
+  // TODO: Change ORDER_NONE to the correct strength.
+  return [code, Blockly.Python.ORDER_NONE];
+};
+
+Blockly.Python['nn_sequential'] = function(block) {
+  var value_name = Blockly.Python.valueToCode(block, 'NAME', Blockly.Python.ORDER_ATOMIC);
+  // TODO: Assemble Python into code variable.
+  var code = 'nn.Sequential(*' + value_name + ')';
+  // TODO: Change ORDER_NONE to the correct strength.
+  return [code, Blockly.Python.ORDER_NONE];
+};
+
+Blockly.Python['nn_forward'] = function(block) {
+  var variable_model = Blockly.Python.variableDB_.getName(block.getFieldValue('model'), Blockly.Variables.NAME_TYPE);
+  var variable_data = Blockly.Python.variableDB_.getName(block.getFieldValue('data'), Blockly.Variables.NAME_TYPE);
+  // TODO: Assemble Python into code variable.
+  var code = variable_model + '(' + variable_data +')';
+  // TODO: Change ORDER_NONE to the correct strength.
+  return [code, Blockly.Python.ORDER_NONE];
+};
+
+Blockly.Python['nn_l1loss'] = function(block) {
+  // TODO: Assemble Python into code variable.
+  var code = 'nn.L1Loss()';
+  // TODO: Change ORDER_NONE to the correct strength.
+  return [code, Blockly.Python.ORDER_NONE];
+};
+
+Blockly.Python['nn_dataset'] = function(block) {
+  var dropdown_name = block.getFieldValue('NAME');
+  // TODO: Assemble Python into code variable.
+  var code = 'bt.Train_Set(' + dropdown_name + ')';
+  // TODO: Change ORDER_NONE to the correct strength.
+  return [code, Blockly.Python.ORDER_NONE];
+};
+
+Blockly.Python['nn_backward'] = function(block) {
+  var variable_model = Blockly.Python.variableDB_.getName(block.getFieldValue('model'), Blockly.Variables.NAME_TYPE);
+  var variable_data = Blockly.Python.variableDB_.getName(block.getFieldValue('data'), Blockly.Variables.NAME_TYPE);
+  // TODO: Assemble Python into code variable.
+  var code = 'bt.train('+ variable_model + ',' + variable_data + ')';
+  return code;
+};
+
+
+Blockly.Python['next'] = function(block) {
+  var variable_dataset = Blockly.Python.variableDB_.getName(block.getFieldValue('dataset'), Blockly.Variables.NAME_TYPE);
+  // TODO: Assemble Python into code variable.
+  var code = 'next(' + variable_dataset + ')';
+  // TODO: Change ORDER_NONE to the correct strength.
+  return [code, Blockly.Python.ORDER_NONE];
+};
